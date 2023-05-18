ada9287b285e4695090e5dac20f9583387cdfa60
302-02 <302-02@DESKTOP-4PSNF9M>
2023-05-18 15:46:35 +0900

    랜덤숫자 출력

---
+++ java_basic/src/com/java/quiz/Random.java
@@ -0,0 +1,13 @@
+package com.java.quiz;
+
+public class Random {
+
+	public static void main(String[] args) {
+
+		int random = (int)(Math.random()*100+1);
+		
+		System.out.println(random);
+	}
+
+}
+//ÅäÅ«: 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
